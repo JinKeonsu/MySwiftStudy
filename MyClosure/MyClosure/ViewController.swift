4a0e482708fba61bf1e839d36dde5fddfc6c7a55
keonsu.jin@gmail.com <keonsu.jin@gmail.com>
2023-03-12 18:18:10 +0900

    2303121818

--- MyClosure/MyClosure/ViewController.swift
+++ MyClosure/MyClosure/ViewController.swift
@@ -62,17 +62,78 @@ class ViewController: UIViewController {
     //MARK: - closure
     func closureTest() {
         
-        // simple closure
+        // 1. simple closure parameter
         let greetUser = { (name: String) in
             print("Hey there, \(name)")
         }
         greetUser("Jin")
         
         
-        // trailing closure
+        // 2. closure that return value
+        var findSquare = { (num: Int) -> Int in
+            var squareValue = num * num
+            return squareValue
+        }
+        var result = findSquare(29)
+        print("square result: \(result)")
         
         
-        print("closure test")
+        // 3. closure as function parameter
+        func grabLunch(search: () -> ()) {
+            print("Let's go out for lunch")
+            
+            search()
+        }
+        
+        func eatPizza() {
+            print("Pizza okay")
+        }
+        
+        func eatSteak() {
+            print("Steak is good")
+        }
+        
+//        grabLunch(search: eatPizza)
+        grabLunch(search: eatSteak)
+        grabLunch(search: { print("direct print statement") })
+        
+        
+        // 4. trailing closure
+        func trailingClosure(message: String, someFunc: ()->() ) {
+            print("1st param message: \(message)")
+            someFunc()
+        }
+        
+        trailingClosure(message: "1st test", someFunc: eatSteak)
+        trailingClosure(message: "2nd test", someFunc: {
+            print("2nd direct print statement")
+        })
+        trailingClosure(message: "3rd test") {
+            print("3rd direct print statement")
+        }
+        
+        
+        // 5. auto closure
+        func normalPrint(_ closure: () -> Void) {
+            closure()
+        }
+        func autoClosurePrint(_ closure: @autoclosure () -> Void) {
+            closure()
+        }
+        normalPrint({ print("I'm normal expression") })
+        autoClosurePrint(print("I'm autoclosure expression"))
+        
+        /*
+         Swift에서 @autoclosure를 사용하는 대표적인 예로 assert()함수가 있습니다.
+         assert() 함수는 대략 다음과 같이 정의 돼 있는데요. (@autoclosure와 상관없는 코드는 생략)
+         public func assert(_ condition: @autoclosure () -> Bool, _ message: @autoclosure () -> String = String())
+         
+         assert(false, "Error Occurred!")
+         assert(0 < 1, "Error Occurred!")
+         assert(false, (statusCode == .fileNotFound) ? "File Not Found!" : "Something going wrong!")
+         */
+
+        
     }
 }
 
